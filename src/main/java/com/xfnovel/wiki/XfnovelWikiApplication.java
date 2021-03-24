e95024642ac4abc519d90fa7d0b3c3c91b99ab92
肖文彭 <ja_xwp@yeah.net>
2021-03-24 14:20:41 +0900

    chore: 初始化项目

---
+++ src/main/java/com/xfnovel/wiki/XfnovelWikiApplication.java
@@ -0,0 +1,13 @@
+package com.xfnovel.wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XfnovelWikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XfnovelWikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xfnovel/wiki/XfnovelWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xfnovel.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XfnovelWikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
